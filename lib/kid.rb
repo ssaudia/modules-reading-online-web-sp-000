16120d23cf74bc0d0061b6a1c61257c606002f6f
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-19 01:23:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,21 +1,2 @@
-class Kid
-  include Dance
-  extend MetaDancing
-  include FancyDance::InstanceMethods
-  extend FancyDance::ClassMethods
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-end
-# ////////////////////////////
-angelina = Kid.new("Angelina")
-mikhail_baryshnikov = Dancer.new("Mikhail")
-
-puts "#{angelina.name} says: #{angelina.twirl}"
-puts "#{mikhail_baryshnikov.name} says: #{mikhail_baryshnikov.take_a_bow}"
-
-puts Kid.metadata
-puts Dancer.metadata
-
-Kid.megajump + " " + angelina.superjump
+require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
