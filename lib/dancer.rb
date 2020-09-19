a0e93f6ff99bce4f9be5576620b519ebcd45b9f5
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-19 01:27:12 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,10 @@
 class Dancer
 
-#We will include our fancy_dance nested modules in the Dancer class. Using the keyword 
+#We will include our fancy_dance nested modules in the Dancer class. Using the keyword
 #"include" allows us to share the modules methods as "instance" methods.
 #We use the keyword "extend" to share a modules methods as "class" methods
-#We use the nested modules by refering to them with the :: notation, which indicates the 
-#Parent::Child relationship of the nested modules 
+#We use the nested modules by refering to them with the :: notation, which indicates the
+#Parent::Child relationship of the nested modules
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
